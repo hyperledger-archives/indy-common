99aab09dc95677255d29b3d593216250894d8c7f
Lovesh <lovesh.harchandani@evernym.com>
2017-03-14 01:32:57 +0530

    completing cleanup method

--- sovrin_common/init_util.py
+++ sovrin_common/init_util.py
@@ -1,13 +1,20 @@
 from plenum.common.init_util import initialize_node_environment as \
     p_initialize_node_environment
+from plenum.persistence.orientdb_store import OrientDbStore
+from sovrin_common.config_util import getConfig
 
 
-def cleanup_environment(name, base_dir):
-    # TODO:
+def cleanup_environment(name, config):
+    o_client = OrientDbStore.new_orientdb_client(user=config.OrientDB["user"],
+                                                 password=config.OrientDB["password"],
+                                                 host=config.OrientDB["host"],
+                                                 port=config.OrientDB["port"],)
+    OrientDbStore.wipe_db(o_client, name)
     pass
 
 
-def initialize_node_environment(name, base_dir, sigseed=None, override=False):
+def initialize_node_environment(name, base_dir, sigseed=None, override=False,
+                                config=None):
     # Question: Does this comment belong here? I dont think so.
     """
     transport-agnostic method; in the future when the transport protocol is
@@ -15,5 +22,6 @@ def initialize_node_environment(name, base_dir, sigseed=None, override=False):
     and likely a method of an interface
     """
     vk = p_initialize_node_environment(name, base_dir, sigseed, override)
+    config = config or getConfig()
+    cleanup_environment(name, config)
     return vk
-
