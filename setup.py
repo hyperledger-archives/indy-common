52d99a7d71f645bfce6e843e4e172bd1065d46c8
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-04-06 14:23:58 +0300

    Versioning fix

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/pool_transactions_sandbox', ])
     )],
-    install_requires=['plenum-dev'],
+    install_requires=['plenum-dev==0.3.23'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     scripts=['scripts/get_keys',
