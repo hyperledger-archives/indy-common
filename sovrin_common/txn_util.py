c0246138e027a3c4ec54d710ca32f310907869a5
Lovesh <lovesh.harchandani@evernym.com>
2017-04-06 17:09:26 +0530

    change in txn ordering type

--- sovrin_common/txn_util.py
+++ sovrin_common/txn_util.py
@@ -113,7 +113,7 @@ def getTxnOrderedFields():
         (f.IDENTIFIER.nm, (str, str)),
         (f.REQ_ID.nm, (str, int)),
         (f.SIG.nm, (str, str)),
-        (TXN_TIME, (str, int)),
+        (TXN_TIME, (str, float)),
         (TXN_TYPE, (str, str)),
         (TARGET_NYM, (str, str)),
         (VERKEY, (str, str)),
