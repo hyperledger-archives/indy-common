b99b564b76555508776c489023956a378884d26c
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-04-18 12:05:45 +0300

    Updated plenum dep

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
 #    data_files=[(
 #        (BASE_DIR, ['data/pool_transactions_sandbox', ])
 #    )],
-    install_requires=['plenum-3pc-batch==0.3.17'],
+    install_requires=['plenum-3pc-batch==0.3.22'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     scripts=['scripts/get_keys',
