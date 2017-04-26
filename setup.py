fafc203139174d8cd24b5f5adc1c079619b618ee
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-04-26 03:23:53 +0300

    increment plenum

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
 #    data_files=[(
 #        (BASE_DIR, ['data/pool_transactions_sandbox', ])
 #    )],
-    install_requires=['plenum-3pc-batch==0.3.67'],
+    install_requires=['plenum-3pc-batch==0.3.70'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     scripts=['scripts/get_keys',
