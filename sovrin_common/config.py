c3181d3008d5e2c11628bf8dddf9f033e886e66a
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-04-27 06:16:32 +0300

    fix idrCacheDb name

--- sovrin_common/config.py
+++ sovrin_common/config.py
@@ -44,6 +44,7 @@ attrStorage = KeyValueStorageType.Leveldb
 
 configStateDbName = 'config_state'
 attrDbName = 'attr_db'
+idrCacheDbName = 'idr_cache_db'
 
 RAETLogLevel = "concise"
 RAETLogLevelCli = "mute"
