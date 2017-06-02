742e0500f9741dfcad97ba84951fcdb50c997e7e
Alexander Shekhovtsov <alexander.shekhovcov@dsr-company.com>
2017-06-02 14:14:13 +0300

    bump plenum-dev==0.3.127 (#88)
    * bump plenum-dev==0.3.126
    
    * bump plenum-dev==0.3.127
    
    * Fix ATTRIB tests

--- sovrin_common/test/types/test_attrib.py
+++ sovrin_common/test/types/test_attrib.py
@@ -8,11 +8,13 @@ from sovrin_common.types import ClientAttribOperation
 
 validator = ClientAttribOperation()
 
+VALID_TARGET_NYM = 'a'*43
+
 
 def test_attrib_with_enc_raw_hash_at_same_time_fails():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '{}',
         ENC: 'foo',
         HASH: 'bar'
@@ -27,7 +29,7 @@ def test_attrib_with_enc_raw_hash_at_same_time_fails():
 def test_attrib_without_enc_raw_hash_fails():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo'
+        TARGET_NYM: VALID_TARGET_NYM
     }
     with pytest.raises(TypeError) as ex_info:
         validator.validate(msg)
@@ -38,7 +40,7 @@ def test_attrib_without_enc_raw_hash_fails():
 def test_attrib_with_raw_string_fails():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: 'foo',
     }
     with pytest.raises(TypeError) as ex_info:
@@ -50,7 +52,7 @@ def test_attrib_with_raw_string_fails():
 def test_attrib_with_raw_empty_json_fails():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '{}',
     }
     with pytest.raises(TypeError) as ex_info:
@@ -62,7 +64,7 @@ def test_attrib_with_raw_empty_json_fails():
 def test_attrib_with_raw_array_fails():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '[]',
     }
     with pytest.raises(TypeError) as ex_info:
@@ -74,7 +76,7 @@ def test_attrib_with_raw_array_fails():
 def test_attrib_with_raw_having_more_one_attrib_fails():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '{"attr1": "foo", "attr2": "bar"}',
     }
     with pytest.raises(TypeError) as ex_info:
@@ -86,7 +88,7 @@ def test_attrib_with_raw_having_more_one_attrib_fails():
 def test_attrib_with_raw_having_one_attrib_passes():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '{"attr1": "foo"}',
     }
     validator.validate(msg)
@@ -95,7 +97,7 @@ def test_attrib_with_raw_having_one_attrib_passes():
 def test_attrib_with_raw_having_endpoint_equal_null_passes():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '{"endpoint": null}',
     }
     validator.validate(msg)
@@ -104,7 +106,7 @@ def test_attrib_with_raw_having_endpoint_equal_null_passes():
 def test_attrib_with_raw_having_endpoint_ha_equal_null_passes():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '{"endpoint": {"ha": null}}',
     }
     validator.validate(msg)
@@ -113,7 +115,7 @@ def test_attrib_with_raw_having_endpoint_ha_equal_null_passes():
 def test_attrib_with_raw_having_endpoint_without_ha_passes():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '{"endpoint": {"foo": "bar"}}',
     }
     validator.validate(msg)
@@ -122,7 +124,7 @@ def test_attrib_with_raw_having_endpoint_without_ha_passes():
 def test_attrib_with_raw_having_endpoint_ha_with_ip_address_only_fails():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '{"endpoint": {"ha": "8.8.8.8"}}',
     }
     with pytest.raises(TypeError) as ex_info:
@@ -134,7 +136,7 @@ def test_attrib_with_raw_having_endpoint_ha_with_ip_address_only_fails():
 def test_attrib_with_raw_having_endpoint_ha_with_invalid_port_fails():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '{"endpoint": {"ha": "8.8.8.8:65536"}}',
     }
     with pytest.raises(TypeError) as ex_info:
@@ -146,7 +148,7 @@ def test_attrib_with_raw_having_endpoint_ha_with_invalid_port_fails():
 def test_attrib_with_raw_having_endpoint_ha_with_invalid_ip_address_fails():
     msg = {
         TXN_TYPE: ATTRIB,
-        TARGET_NYM: 'foo',
+        TARGET_NYM: VALID_TARGET_NYM,
         RAW: '{"endpoint": {"ha": "256.8.8.8:9700"}}',
     }
     with pytest.raises(TypeError) as ex_info:
